af22055aa437d607ea0ea4486bbabc1d8a9e44cc
lucasd4257 <lucasd4257@gmail.com>
2026-05-02 18:01:38 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int orginalValue, Product product)
+        {
+            int copiaValue = orginalValue;
+            copiaValue++;
+
+            Product copiaProduct = product;
+
+            copiaProduct.SetDescription("Modificado");
+
+            return $"{orginalValue}-{copiaValue}-{product.GetDescription}";
+        }
+    }
+
+    internal class Product
+    {
+
+        private string description = "Original";
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public void SetDescription(string description)
+        {
+            this.description = description;
+        }
+
+    }
+}
