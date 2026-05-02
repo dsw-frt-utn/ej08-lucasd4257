c6ead7211aff1ed9ddcfb8fe395e61cfebddef98
lucasd4257 <lucasd4257@gmail.com>
2026-05-02 18:12:27 -0300

    Problema 5 resuelto

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+
+    internal class Sale
+    {
+        protected decimal numero;
+
+        public Sale(decimal numero)
+        {
+            this.numero = numero;
+        }
+
+        public virtual decimal CalculateTotal()
+        {
+            return numero;
+        }
+    }
+
+    internal class RetailSale : Sale
+    {
+        public RetailSale(decimal numero) : base(numero)
+        {
+        }
+        public override decimal CalculateTotal()
+        {
+            return numero;
+        }
+    }
+
+    internal class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal numero) : base(numero)
+        {
+        }
+        public override decimal CalculateTotal()
+        {
+            return numero * (decimal)0.9;
+        }
+    }
+}
