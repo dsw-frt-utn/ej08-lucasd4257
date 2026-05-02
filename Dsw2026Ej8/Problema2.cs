cf4ff85d02ef9fb09ed9f2a6ececa37533bedccd
lucasd4257 <lucasd4257@gmail.com>
2026-05-02 18:00:21 -0300

    Problema 2 resuelto

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+
+            if (quantity <= 0)
+            {
+                total = 0;
+            }
+            else
+            {
+                total = quantity * unitPrice;
+            }
+
+            var resumen = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total};
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
