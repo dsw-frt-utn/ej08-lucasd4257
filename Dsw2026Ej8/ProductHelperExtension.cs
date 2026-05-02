04bad59cdd312125ea8b081b282789b8ff9b47c2
lucasd4257 <lucasd4257@gmail.com>
2026-05-02 17:59:52 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/ProductHelperExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string PrecioMoneda(decimal price)
+        {
+            return $"{price:C2}";
+        }
+    }
+}
