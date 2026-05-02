efdabb80439ad0e8ea665a4a3d95facdf5f0fda9
lucasd4257 <lucasd4257@gmail.com>
2026-05-02 19:00:31 -0300

    Problema 6 resuelto

---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.Schema;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            
+            return code.ToProductCode();
+        }
+    }
+    
+}
